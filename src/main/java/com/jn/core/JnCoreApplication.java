ab2ecda0adad2a8e76948ec5d996810b83d567b7
Huang ChenGang <hcggang@163.com>
2020-12-20 13:28:33 +0800

    publish success

---
+++ src/main/java/com/jn/core/JnCoreApplication.java
@@ -0,0 +1,13 @@
+package com.jn.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JnCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JnCoreApplication.class, args);
+    }
+
+}
