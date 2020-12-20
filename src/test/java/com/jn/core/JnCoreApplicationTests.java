46d2abfa1d98c46d4cb3d577e399253cf5414e7c
Huang ChenGang <hcggang@163.com>
2020-12-20 11:33:39 +0800

    创建项目

---
+++ src/test/java/com/jn/core/JnCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jn.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JnCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
