78d479be5d2134244556b87a71cd6e8240170893
Huang ChenGang <hcggang@163.com>
2021-03-07 21:22:29 +0800

    更新JnPageResponse

--- jn-core-common/src/main/java/com/jn/core/api/JnPageResponse.java
+++ jn-core-common/src/main/java/com/jn/core/api/JnPageResponse.java
@@ -1,17 +1,30 @@
 package com.jn.core.api;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
+import org.springframework.data.domain.Page;
 
 import java.util.List;
 
 @Data
-@AllArgsConstructor
 public class JnPageResponse<T> {
     private List<T> list;
-    private long totalElements;
 
-    public static <T> JnPageResponse<T> of(List<T> list, long totalElements) {
-        return new JnPageResponse<>(list, totalElements);
+    private Integer totalPages;
+    private Long totalElements;
+    private Boolean first;
+    private Boolean last;
+    private Boolean previous;
+    private Boolean next;
+
+    public static <T> JnPageResponse<T> of(Page<T> page) {
+        JnPageResponse<T> jnPageResponse = new JnPageResponse<>();
+        jnPageResponse.setFirst(page.isFirst());
+        jnPageResponse.setLast(page.isLast());
+        jnPageResponse.setPrevious(page.hasPrevious());
+        jnPageResponse.setNext(page.hasNext());
+        jnPageResponse.setTotalPages(page.getTotalPages());
+        jnPageResponse.setTotalElements(page.getTotalElements());
+        jnPageResponse.setList(page.getContent());
+        return jnPageResponse;
     }
 }
